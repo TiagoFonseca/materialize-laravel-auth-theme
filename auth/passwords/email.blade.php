2f0a6cce1d992488783cd8464aa96b29669cd66f
gangachris <ganga.chris@gmail.com>
2016-03-06 11:14:57 +0300

    Password reset email

--- auth/passwords/email.blade.php
+++ auth/passwords/email.blade.php
@@ -2,46 +2,37 @@
 
 <!-- Main Content -->
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i>Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="center">
+  <div class="row form z-depth-1">
+        <h3 style="font-weight: 100">Reset Password</h3>
+        @if (session('status'))
+          <div class="card-panel green lighten-2">
+            <p class="white-text text-darken-2">{{ $error }}</p>
+          </div>
+        @endif
+        @if (count($errors) > 0)
+          <ul class="card-panel red lighten-2">
+              @foreach($errors->all() as $error)
+                  <li class="white-text text-darken-2">{{ $error }}</li>
+              @endforeach
+          </ul>
+        @endif
+        <form role="form" method="POST" action="{{ url('/password/email') }}">
+          {!! csrf_field() !!}
+          <div class="row">
+            <div class="input-field col s12">
+              <input type="email" name="email" type="text">
+              <label for="email">Email Address</label>
             </div>
-        </div>
-    </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12 center">
+                <button type="submit" class="btn">
+                    <i class="fa fa-btn fa-sign-in"></i>Send Password Reset Link
+                </button>
+            </div>
+          </div>
+         </form>
+     </div>
 </div>
 @endsection
