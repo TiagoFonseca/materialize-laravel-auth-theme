57f725f05a3d3c5bb14e1ee1366daf47d1546b90
gangachris <ganga.chris@gmail.com>
2016-03-06 11:43:13 +0300

    Home and welcome pages

---
+++ welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <span class="flow-text">Welcome</span>
+        <p>Your applications landing page</p>
+    </div>
+</div>
+@endsection
